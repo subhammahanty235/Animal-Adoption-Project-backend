31c982b9d6984fc8c02c749819d6c484ad2f7e46
subhammahanty235 <subhammahanty235@gmail.com>
2023-02-28 23:32:45 +0530

    Project structure

---
+++ app.js
@@ -0,0 +1,42 @@
+
+require('dotenv').config()
+const express = require('express');
+const mongoose = require('mongoose');
+
+const cors = require('cors');
+const connectToDB = require('./db');
+const app = express();
+
+app.use(express.json());
+connectToDB();
+const whitelist = ["http://localhost:3000"]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (!origin || whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error("Not allowed by CORS"))
+    }
+  },
+  credentials: true,
+}
+app.use(cors(corsOptions))
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
+// app.use('/api/v1' , require('./routes/userRoutes'))
+// app.use('/api/v1' , require('./routes/tripRoutes'))
+// app.use('/api/v1',require('./routes/emailroutes'))
+
+app.get('/',(req,res)=>{
+    res.send("Animal Adoption Project")
+})
+const port = process.env.PORT || 5000
+app.listen(port , ()=>{
+    console.log("Running on port"+port)
+})
