d55db14be84084d2c9ba08099ac3e0a0761decec
subhammahanty235 <subhammahanty235@gmail.com>
2023-03-01 00:38:15 +0530

    user and pet model

---
+++ model/pet.model.js
@@ -0,0 +1,82 @@
+const mongoose = require('mongoose')
+
+const PetSchema = mongoose.Schema({
+    postBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    location:{
+        country:{
+            type:String,
+            required:true
+        },
+        state:{
+            type:String,
+            required:true
+        },
+        city:{
+            type:String,
+            required:true
+        },
+        coords:{
+            longitude:{
+                type:String,
+                
+            },
+            latitude:{
+                type:String,
+            
+            }
+        },
+        pincode:{
+            type:Number,
+            required:true,
+            min:4,
+            max:8
+        }
+
+    },
+    
+    name: {
+        type: String,
+        required: true,
+    },
+    about:{
+
+    },
+    type: {
+        type: String,
+
+    },
+
+    createdAt: {
+        type: Date,
+        default: Date.now()
+    },
+    status: {
+        type: Number,
+        deafult: 1,          //1 for not  adopted yet , 2 for adopted
+    },
+    views: {
+        type: Number,
+        default: 0,
+    },
+    interests: {
+        type: [String]
+    },
+    age: {
+        type: Number,
+    },
+    breed: {
+        type: String,
+    },
+    vaccinated: {
+        type: Boolean,
+        default: false,
+    },
+
+
+
+
+
+})
---
+++ model/user.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const UserSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        min:3,
+        max:25,
+    },
+    email:{
+        type:String,
+        required:true
+        
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    isrescuer:{
+        type:Boolean,
+        default:false,
+    },
+    isnormaluser:{
+        type:Boolean,
+        default:false
+    }
+
+})
+
+const User = mongoose.model('user' , UserSchema)
+module.exports = User
