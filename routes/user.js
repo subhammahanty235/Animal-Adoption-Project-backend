fe89315799bf14c6941e90f3a55264569612b179
Subham Mahanty <99578301+subhammahanty235@users.noreply.github.com>
2023-03-05 22:36:08 +0530

    added routes

--- routes/user.js
+++ routes/user.js
@@ -1,1 +1,9 @@
-pet
+const router = require('express').Router()
+const fetchUser = require('../middleware/fetchuser.middleware')
+const normalUserFunctions = require('../controllers/user.controller')
+
+router.post('/register' , fetchUser , normalUserFunctions.registerasUser)
+router.put('/edituser' , fetchUser , normalUserFunctions.editNormalUserDetails)
+router.get('/getuser' , fetchUser , normalUserFunctions.getUserData)
+
+module.export = router
